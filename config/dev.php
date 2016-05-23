73f990488d184b9fa0033d199df5424868aa3ada
Ryan Carr <ryancarr10@gmail.com>
2016-05-23 02:27:17 -0400

    Updated mode files

---
+++ config/dev.php
@@ -0,0 +1,39 @@
+<?php
+return [
+    'app' => [
+        'url' => '',
+        'hash' => [
+            'algo' => PASSWORD_BCRYPT,
+            'cost' => 10
+        ]
+    ],
+    'db' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'name' => 'site',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => ''
+    ]
+    'auth' => [
+        'session' => 'user_id',
+        'remember' => 'user_r'
+    ],
+    'mail' => [
+        'smtp_auth' => true,
+        'smtp_secure' => '',
+        'host' => '',
+        'username' => '',
+        'password' => '',
+        'port' => 587,
+        'html' => true
+    ],
+    'twig' => [
+      'debug' => true  
+    ],
+    'csrf' => [
+        'session' => 'csrf_token'
+    ]
+];
